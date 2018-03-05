ee3b9d1862add2bed8d74b0a4d3ee30ab4f39a0e
Ilya Bizyaev <bizyaev@zoho.com>
2018-03-05 23:50:35 +0300

    Basic buildable structure

--- biginteger.cpp
+++ biginteger.cpp
@@ -1,53 +1,97 @@
 #include "biginteger.hpp"
+#include <stdlib.h>
+#include <sstream>
 
 //
 // Class implementation
 //
 
-BigInteger::BigInteger(long long value)
+BigInteger::BigInteger(long long value) : m_negative(value < 0)
 {
-    // TODO
+    if (m_negative) {
+        value = -value;
+    }
+    std::stringstream ss;
+    ss << value;
+    m_number = ss.str();
+    std::cout << "Constructed: " << (m_negative ? '-' : '+') << m_number << std::endl;
 }
 
 BigInteger::BigInteger(const BigInteger & bigint)
 {
-    // TODO
+    m_number = bigint.m_number;
+    m_negative = bigint.m_negative;
 }
 
 BigInteger::BigInteger(BigInteger && bigint)
 {
-    // TODO
+    m_number = std::string(std::move(bigint.m_number));
+    m_negative = bigint.m_negative;
 }
 
-BigInteger & BigInteger::operator +=(const BigInteger &)
+BigInteger& BigInteger::operator =(const BigInteger & other)
 {
-    // TODO
-}
-BigInteger BigInteger::operator +(const BigInteger &) const
-{
-    // TODO
+    this->m_number = other.m_number;
+    this->m_negative = other.m_negative;
+    return *this;
 }
 
-BigInteger & BigInteger::operator -=(const BigInteger &)
+BigInteger & BigInteger::operator +=(const BigInteger &other)
 {
-    // TODO
+    return *this = *this + other;
+}
+BigInteger BigInteger::operator +(const BigInteger &other) const
+{
+    // size_t n = std::max(this->m_number.length(), other.m_number.length())+1;
+
+    if (this->m_negative && other.m_negative) {
+        // neg + neg = neg(sum);
+        return -(-(*this) + (-other));
+    } else if (this->m_negative && !other.m_negative) {
+        // neg + pos = -subtraction
+        return -(-(*this)+(-other));
+    } else if (!this->m_negative && !other.m_negative) {
+        // pos + pos = sum
+        // TODO
+        int carry = 0;
+        return BigInteger(42);
+    } else {
+        // pos + neg = subtraction
+        // TODO
+        int carry = 0;
+        return BigInteger(42);
+    }
 }
 
-BigInteger BigInteger::operator -(const BigInteger &) const
+BigInteger & BigInteger::operator -=(const BigInteger &other)
 {
-    // TODO
+    return *this = *this - other;
+}
+
+BigInteger BigInteger::operator -(const BigInteger &other) const
+{
+    return *this + (-other);
+}
+
+BigInteger BigInteger::operator -() const
+{
+    BigInteger temp = *this;
+    temp.m_negative = !(this->m_negative);
+    return temp;
 }
 
 //
 // Input-output stream implementations
 //
 
-std::ostream & operator <<(std::ostream &, const BigInteger &)
+std::ostream & operator <<(std::ostream &stream, const BigInteger &bigint)
 {
-    // TODO
+    stream << bigint.m_number;
+    return stream;
 }
 
-std::istream & operator >>(std::ostream &, BigInteger &)
+std::istream & operator >>(std::istream &stream, BigInteger &bigint)
 {
-    // TODO
+    stream >> bigint.m_number;
+    return stream;
 }
--- biginteger.hpp
+++ biginteger.hpp
@@ -2,25 +2,30 @@
 #define BIGINTEGER_H
 
 #include <iostream>
+#include <string>
 
 class BigInteger {
+    friend std::ostream & operator <<(std::ostream &, const BigInteger &);
+    friend std::istream & operator >>(std::istream &, BigInteger &);
 public:
 	explicit BigInteger(long long value = 0);
 	BigInteger(const BigInteger &);
 	BigInteger(BigInteger &&);
+    BigInteger& operator =(const BigInteger &);
 
 	BigInteger & operator +=(const BigInteger &);
 	BigInteger operator +(const BigInteger &) const;
 
 	BigInteger & operator -=(const BigInteger &);
 	BigInteger operator -(const BigInteger &) const;
+    BigInteger operator -() const;
 
 private:
-	friend std::ostream & operator <<(std::ostream &, const BigInteger &);
-	friend std::istream & operator >>(std::ostream &, BigInteger &);
+    std::string m_number;
+    bool m_negative;
 };
 
 std::ostream & operator <<(std::ostream &, const BigInteger &);
-std::istream & operator >>(std::ostream &, BigInteger &);
+std::istream & operator >>(std::istream &, BigInteger &);
 
 #endif // BIGINTEGER_H
