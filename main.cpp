139dc9e897ba8edd50e271bd38c15cdc77f3ea60
Ilya Bizyaev <bizyaev@zoho.com>
2018-03-14 14:05:33 +0300

    2 tests

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main()
 {
     BigInteger a, b;
     std::cin >> a >> b;
-    std::cout << a + b << std::endl;
+
+    std::cout << "a + b: " << a + b << std::endl;
+    std::cout << "a - b: " << a - b << std::endl;
     return 0;
 }
